e2b8fd29d0894bd5bd9f1dbe6ad67f72e5139cdc
harper71 <princeazuka99@gmail.com>
2024-10-17 00:07:02 +0100

    commit changes

--- 0x00-MySQL_Advanced/checks.sql
+++ 0x00-MySQL_Advanced/checks.sql
@@ -1,6 +1,12 @@
 SELECT * FROM users;
 
-INSERT INTO users (id, email, name) VALUES (1, "sylvie@dylan.com", "Sylvie");
+INSERT INTO users (id, email, name, country) VALUES (1, "sylvie@dylan.com", "Sylvie", 'CO');
 INSERT INTO users (id,email, name) VALUES (2, "bob@dylan.com", "Bob");
 
+INSERT INTO users (email, name, country) VALUES ("jean@dylan.com", "Jean", "FR");
+
+INSERT INTO users (email, name) VALUES ("john@dylan.com", "John");
+
 DELETE FROM users;
+
+DROP TABLE users;
