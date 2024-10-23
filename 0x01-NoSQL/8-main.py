b05ba9b4e2f3d1f307df13367f92e4cd2f6a153b
harper71 <princeazuka99@gmail.com>
2024-10-23 02:38:04 +0100

    commit data

--- 0x01-NoSQL/8-main.py
+++ 0x01-NoSQL/8-main.py
@@ -1,28 +1,20 @@
 #!/usr/bin/env python3
-""" 101-main """
+""" 10-main """
 from pymongo import MongoClient
 list_all = __import__('8-all').list_all
-insert_school = __import__('9-insert_school').insert_school
-top_students = __import__('101-students').top_students
+update_topics = __import__('10-update_topics').update_topics
 
 if __name__ == "__main__":
     client = MongoClient('mongodb://127.0.0.1:27017')
-    students_collection = client.my_db.students
+    school_collection = client.my_db.school
+    update_topics(school_collection, "Holberton school", ["Sys admin", "AI", "Algorithm"])
 
-    j_students = [
-        { 'name': "John", 'topics': [{ 'title': "Algo", 'score': 10.3 },{ 'title': "C", 'score': 6.2 }, { 'title': "Python", 'score': 12.1 }]},
-        { 'name': "Bob", 'topics': [{ 'title': "Algo", 'score': 5.4 },{ 'title': "C", 'score': 4.9 }, { 'title': "Python", 'score': 7.9 }]},
-        { 'name': "Sonia", 'topics': [{ 'title': "Algo", 'score': 14.8 },{ 'title': "C", 'score': 8.8 }, { 'title': "Python", 'score': 15.7 }]},
-        { 'name': "Amy", 'topics': [{ 'title': "Algo", 'score': 9.1 },{ 'title': "C", 'score': 14.2 }, { 'title': "Python", 'score': 4.8 }]},
-        { 'name': "Julia", 'topics': [{ 'title': "Algo", 'score': 10.5 },{ 'title': "C", 'score': 10.2 }, { 'title': "Python", 'score': 10.1 }]}
-    ]
-    for j_student in j_students:
-        insert_school(students_collection, **j_student)
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
 
-    students = list_all(students_collection)
-    for student in students:
-        print("[{}] {} - {}".format(student.get('_id'), student.get('name'), student.get('topics')))
+    update_topics(school_collection, "Holberton school", ["iOS"])
 
-    top_students = top_students(students_collection)
-    for student in top_students:
-        print("[{}] {} => {}".format(student.get('_id'), student.get('name'), student.get('averageScore')))
+    schools = list_all(school_collection)
+    for school in schools:
+        print("[{}] {} {}".format(school.get('_id'), school.get('name'), school.get('topics', "")))
